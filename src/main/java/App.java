1ad3cbe119c173a9babc87db6123515241e2b3dd
devivasha <nat_ivaniv@ukr.net>
2020-09-13 10:34:34 +0300

    commit empty pro

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
-public class App {
-    public static void main(String[] args) {
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlet.IndexServlet;
+import servlet.RedirectServlet;
 
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+//        Storage storage = new SQLStorage();
+        Server server = new Server(8088);
+        ServletContextHandler handler = new ServletContextHandler();
+        handler.addServlet(IndexServlet.class, "/users");
+        handler.addServlet(RedirectServlet.class, "/*");
+        server.setHandler(handler);
+        server.start();
+        server.join();
     }
 }
